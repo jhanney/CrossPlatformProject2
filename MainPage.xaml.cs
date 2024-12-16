df621548a38ad17ace205928b07613fe5d6efcbd
JoshuaHanney <163124064+JoshuaHanney@users.noreply.github.com>
2024-12-16 16:23:03 +0000

    image rotate throwing exception, made new method to begin rotation only after image is loaded correctly

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,14 +3,18 @@ namespace CrossPlatformProject2
 {
     public partial class MainPage : ContentPage
     {
-       
-
         public MainPage()
         {
             InitializeComponent();
-           
+
+            //planetRotation();
+        }
+
+        private void OnGalaxyImageLoaded(object sender, EventArgs e)//only beigin rotation after image is loaded, found thread on error i was getting more info in ReadMe
+        {
             planetRotation(); 
         }
+
 
         private async void planetRotation() //method to rorate the planet
         {
@@ -18,8 +22,11 @@ namespace CrossPlatformProject2
             {
                 while (true)
                 {
-                    await galaxyImage.RelRotateTo(360, 20000, Easing.Linear); //try rotation
-                    galaxyImage.Rotation = 0; //reset rotation
+                    await MainThread.InvokeOnMainThreadAsync(async () =>
+                    {
+                         galaxyImage.RelRotateTo(360, 5000, Easing.Linear);
+                         galaxyImage.Rotation = 0; //reset rotation 
+                    });
                 }
             }
             catch (Exception ex)
@@ -29,6 +36,8 @@ namespace CrossPlatformProject2
                 await DisplayAlert("Error", "Planet rotation failed. Please restart the app.", "OK");//display image to show rotation not working
             }
         }
+
+        
 
         private async void Leaderboard_Clicked(object sender, EventArgs e) 
         {
