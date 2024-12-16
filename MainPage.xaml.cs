3082f211c18f161a5cb476d9ac4d19a5fd3dae49
JoshuaHanney <163124064+JoshuaHanney@users.noreply.github.com>
2024-12-16 13:24:03 +0000

    added rotating planet method

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,9 +7,18 @@
         public MainPage()
         {
             InitializeComponent();
+            planetRotation(); 
         }
 
-       
+        private async void planetRotation() //method to rorate the planet
+        {
+            while (true)//keeps planet constaltly rotating 
+            {
+                await planetImage.RelRotateTo(360, 10000, Easing.Linear); 
+                planetImage.Rotation = 0; 
+            }
+        }
+
     }
 
 }
