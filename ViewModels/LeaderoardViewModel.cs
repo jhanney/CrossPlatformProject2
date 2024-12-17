3c6597dc08282c00aa98b57871c615a7a093699b
JoshuaHanney <163124064+JoshuaHanney@users.noreply.github.com>
2024-12-17 12:10:12 +0000

    add leaderboard ViewModel with obersvableCollection for player scores

---
+++ ViewModels/LeaderoardViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossPlatformProject2.ViewModels
+{
+    class LeaderboardViewModel
+    {
+        public ObservableCollection<ScoreEntry> Scores { get; set; }
+
+        public class ScoreEntry
+        {
+            public string playerName { get; set; }
+            public int score { get; set; }
+        }
+    }
+}
