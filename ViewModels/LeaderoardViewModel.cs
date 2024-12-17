c4a78f8dd9baea63020e38cf17b0e0f435447d46
JoshuaHanney <163124064+JoshuaHanney@users.noreply.github.com>
2024-12-17 12:17:42 +0000

    Added placeholder data

--- ViewModels/LeaderoardViewModel.cs
+++ ViewModels/LeaderoardViewModel.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace CrossPlatformProject2.ViewModels
 {
     class LeaderboardViewModel
     {
-        public ObservableCollection<ScoreEntry> Scores { get; set; }
+        public ObservableCollection<ScoreEntry> Scores { get; set; } //observalble collection to store scores
 
-        public class ScoreEntry
+        public LeaderboardViewModel()
+        {
+            Scores = new ObservableCollection<ScoreEntry>();
+            {
+                //placeholder data
+                new ScoreEntry { playerName = "Josh", score = 100 };
+                new ScoreEntry { playerName = "Trish", score = 90 };
+                new ScoreEntry { playerName = "Joe", score = 80 };
+
+            }
+        }
+
+        public class ScoreEntry //class represents data entered for a score 
         {
             public string playerName { get; set; }
             public int score { get; set; }
