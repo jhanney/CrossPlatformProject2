84bc8e1bc59afaf7a6b9928f243f5a37413ce360
JoshuaHanney <163124064+JoshuaHanney@users.noreply.github.com>
2024-12-17 17:40:17 +0000

    add achievment model with title,description and isUnlcoked

--- ViewModels/AchievementsViewModel.cs
+++ ViewModels/AchievementsViewModel.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CrossPlatformProject2.ViewModels
 {
-    class AchievementsViewModel
+    public class AchievementsViewModel
     {
+        public ObservableCollection
+
+
+    
+    }
+    public class Achievment
+    {
+        public string Title { get; set; } //title of the achievement
+        public string Description { get; set; }//achievment description
+        public bool IsUnlocked { get; set; }//whther its been completed
     }
 }
