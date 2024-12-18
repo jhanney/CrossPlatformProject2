e0a529d131054e52de9df466c9ef5ebd6b701500
JoshuaHanney <163124064+JoshuaHanney@users.noreply.github.com>
2024-12-18 16:26:08 +0000

    add option for up to 4 players

--- GameSetup.xaml.cs
+++ GameSetup.xaml.cs
@@ -5,5 +5,11 @@ public partial class GameSetup : ContentPage
 	public GameSetup()
 	{
 		InitializeComponent();
-	}
+        //add options for player selection
+		playerPicker.Items.Add("1 Player");
+        playerPicker.Items.Add("2 Player");
+        playerPicker.Items.Add("3 Player");
+        playerPicker.Items.Add("4 Player");
+
+    }
 }
