51ff5e13af358435cacd3a0516734e773dd6c4bb
JoshuaHanney <163124064+JoshuaHanney@users.noreply.github.com>
2024-12-23 16:18:27 +0000

    add game state model to save game

---
+++ Models/GameState.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace CrossPlatformProject2.Models
+{
+    public class GameState //game state page to save
+    {
+        public Dictionary<string, int> PlayerScores { get; set; }
+        public int CurrentQuestionIndex { get; set; }
+        public List<QuestionModel> RemainingQuestions { get; set; }
+        public int CurrentPlayerIndex { get; set; }
+        public string SelectedDifficulty { get; set; }
+        public string SelectedCategory { get; set; }
+        public List<string> PlayerNames { get; set; }
+    }
+}
