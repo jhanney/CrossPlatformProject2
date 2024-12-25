689ce73eb408e022100cb29a77399caa5d3c7e74
JoshuaHanney <163124064+JoshuaHanney@users.noreply.github.com>
2024-12-25 12:35:32 +0000

    method to update leaderboard

--- ViewModels/LeaderoardViewModel.cs
+++ ViewModels/LeaderoardViewModel.cs
@@ -8,8 +8,12 @@ namespace CrossPlatformProject2.ViewModels
     {
         public ObservableCollection<ScoreEntry> Scores { get; set; } //observalble collection to store scores
 
+        private static readonly string LeaderboardFilePath = Path.Combine(FileSystem.AppDataDirectory, "LeaderboardData.json");//define file path to save scores
+
         public LeaderboardViewModel(Dictionary<string, int> playerScores)
         {
+
+
             Scores = new ObservableCollection<ScoreEntry>(
                playerScores
                    .OrderByDescending(score => score.Value)//sort score in descending order
@@ -18,6 +22,11 @@ namespace CrossPlatformProject2.ViewModels
                        playerName = score.Key,
                        score = score.Value
                    }));
+        }
+
+        public void AddOrUpdateScore()
+        {
+
         }
 
         public class ScoreEntry //class represents data entered for a score 
