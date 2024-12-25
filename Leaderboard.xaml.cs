08069dd64174e1a61687423be58bea4a3bbe4f6f
JoshuaHanney <163124064+JoshuaHanney@users.noreply.github.com>
2024-12-25 11:15:08 +0000

    update leader board constructor

--- Leaderboard.xaml.cs
+++ Leaderboard.xaml.cs
@@ -4,12 +4,13 @@ using CrossPlatformProject2.ViewModels; //initialise the view models folder
 public partial class Leaderboard : ContentPage
 {
     private MainPage mainPage; 
-	public Leaderboard(MainPage mainPage)
+	public Leaderboard(MainPage mainPage, Dictionary<string, int> playerScores)
 	{
 		InitializeComponent(); 
         this.mainPage = mainPage;//store the reference to mainpage 
-        BindingContext = new LeaderboardViewModel(); //set the binding context to leaderboardView model
-	}
+        // Initialize ViewModel with dynamic player scores
+        BindingContext = new LeaderboardViewModel(playerScores); //set the binding context to leaderboardView model
+    }
 
     private void homeButton_Clicked(object sender, EventArgs e)//navigate back to home page
     {
